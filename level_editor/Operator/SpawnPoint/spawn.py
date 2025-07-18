697d2e2e45a236e97b3d73e4e2f93d0a73c58297
yuzuki5695 <146906397+yuzuki5695@users.noreply.github.com>
2025-07-18 12:58:29 +0900

    それぞれの出現シンボルの作成

--- level_editor/Operator/SpawnPoint/spawn.py
+++ level_editor/Operator/SpawnPoint/spawn.py
@@ -63,3 +63,28 @@ class MYADDON_OT_spawn_import_symbol(bpy.types.Operator):
         self.load_obj("Player")
 
         return{'FINISHED'}
+    
+# オペレータ 出現ポイントのシンボルを作成・配置する
+class MYADDON_OT_spawn_create_symbol(bpy.types.Operator):
+    bl_idname ="myaddon.myaddon_ot_spawn_create_symbol"
+    bl_label = "出現ポイントシンボルの作成"
+    bl_description = "出現ポイントのシンボルを作成します"
+    bl_options = {'REGISTER', 'UNDO'}
+
+    #プロパティ(引数として渡せる)
+    type: bpy.props.StringProperty(name = "Type", default = "Player")
+    
+
+
+    def execute(self,context):
+        # 読み込み済みのコピー元オブジェクトを検索
+        spawn_object = bpy.data.objects.get(SpawnNames.names[self.type][SpawnNames.PROTOTYPE])
+
+        # まだ読み込んでいない場合
+        if spawn_object is None:
+            # 読み込みオペレータを実行
+            bpy.ops.myaddon.myaddon_ot_spawn_import_symbol('INVOKE_DEFAULT')
+            # 再検索。今度は見つかるはず
+            spawn_object = bpy.data.objects.get(SpawnNames.names[self.type][SpawnNames.PROTOTYPE])
+        
+        print("出現ポイントのシンボルを作成します")
