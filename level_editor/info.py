d322e5a145fd3964e2d71c0f9d8a57dba592cef7
yuzuki5695 <146906397+yuzuki5695@users.noreply.github.com>
2025-08-05 14:10:55 +0900

    アドオン情報用のクラスを設計

---
+++ level_editor/info.py
@@ -0,0 +1,15 @@
+
+
+ # ブレンダーに登録するアドオン情報
+bl_info = {
+    "name": "レベルエディタ",
+    "author": "Taro kamata",
+    "version": (1, 0),
+    "blender": (3, 3, 1),
+    "location": "",
+    "description": "レベルエディタ",
+    "warning": "",
+    "wiki_url": "",      
+    "tracker_url": "",
+    "category": "Object"
+}
